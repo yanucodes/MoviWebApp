44b449e19fe4be66e61ae01eb725cc811f0724c8
Yanu <59870900+khusanova@users.noreply.github.com>
2026-05-08 19:32:22 +0200

    Set up database configuration in app.py

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from data_manager import DataManager
+from models import db, Movie, User
+from config import get_db_path
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{get_db_path()}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+data_manager = DataManager()
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    return 'Welcome to MoviWeb App!'
 
 
 if __name__ == '__main__':
