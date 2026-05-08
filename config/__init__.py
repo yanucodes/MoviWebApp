1f9bea9bb7113e554c85f3e008f2d2b182bfc8aa
Yanu <59870900+khusanova@users.noreply.github.com>
2026-05-08 19:47:59 +0200

    Remove depricated variables from config

--- config/__init__.py
+++ config/__init__.py
@@ -4,13 +4,11 @@ from .config import (YEAR_MIN, YEAR_MAX, LOWEST_RATING, HIGHEST_RATING,
                      ROUND_NDIGITS, API_ENV_KEY, API_URL, get_api_key,
                      DB_ENV_KEY, DEFAULT_DBPATH, get_db_path, ENV_FILE,
                      HTML_TEMPLATE, REPLACE_TITLE, REPLACE_GRID,
-                     MOVIES_HTML_PATH, DEFAULT_TITLE, TABS_NUM,
-                     JSON_STORAGE_PATH)
+                     MOVIES_HTML_PATH, DEFAULT_TITLE, TABS_NUM)
 
 
 __all__ = ["YEAR_MIN", "YEAR_MAX", "LOWEST_RATING", "HIGHEST_RATING",
            "ROUND_NDIGITS", "API_ENV_KEY", "API_URL", "get_api_key",
            "DB_ENV_KEY", "DEFAULT_DBPATH", "get_db_path", "ENV_FILE",
            "HTML_TEMPLATE", "REPLACE_TITLE", "REPLACE_GRID",
-           "MOVIES_HTML_PATH", "DEFAULT_TITLE", "TABS_NUM",
-           "JSON_STORAGE_PATH"]
+           "MOVIES_HTML_PATH", "DEFAULT_TITLE", "TABS_NUM"]
