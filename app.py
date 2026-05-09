3952232ce8025348f7db91066250f6154408f72a
Yanu <59870900+khusanova@users.noreply.github.com>
2026-05-09 15:20:40 +0200

    Add route list_users

--- app.py
+++ app.py
@@ -15,5 +15,11 @@ def hello_world():
     return 'Welcome to MoviWeb App!'
 
 
+@app.route('/users')
+def list_users():
+    users = data_manager.get_users()
+    return str(users)  # Temporarily returning users as a string
+
+
 if __name__ == '__main__':
     app.run()
